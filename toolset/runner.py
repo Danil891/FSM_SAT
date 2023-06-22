ede9f2c136b6c2b22f6365a947126bbe8a14015c
Danil891 <cherepkov.dd@edu.spbstu.ru>
2023-06-22 11:14:09 +0300

    right version

--- toolset/runner.py
+++ toolset/runner.py
@@ -2,14 +2,10 @@ import subprocess
 import os
 import shutil
 
-os.chdir('trace_generator')
-subprocess.call(['java', '-jar', 'trace_generator.jar', 'dat', 'tb_diff_TDM'])
-os.chdir('../')
-
 shutil.copy('trace_generator/output_trace/formed_trace.txt', 'SAT_invocator/formed_trace.txt')
 
 os.chdir('SAT_invocator')
-subprocess.Popen("python main.py", shell=True)
+subprocess.Popen("python main2.py", shell=True)
 os.chdir('../')
 
 shutil.copy('SAT_invocator/sat_to_genFSM.txt', 'FSM_generator/input.txt')
